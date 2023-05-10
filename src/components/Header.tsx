35aa4d4e19aaba549a2282f50d0761ce639db1a7
Adaeze Ndupu <andupu4@gmail.com>
2023-05-10 22:48:42 +0100

    refactor: refactor home page to accomodate fixes and remove duplicate header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,59 @@
-import HamburgerIcon from "../assets/hamburger.svg";
-import Logo from "../assets/images/codespace-logo.svg";
+import { useState, useEffect } from "react";
+import HamburgerIcon from "./HamburgerIcon";
+import logo from "../assets/logo.svg";
 import { menuItems } from "../data/menuItems";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 42) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const handleNavClick = () => {
+    setNavOpen(!navOpen);
+    setIsAnimating(!isAnimating);
+  };
+  const navClasses = `fixed top-0 h-full w-full bg-white transform transition-all ease-in-out duration-500 ${
+    navOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+  }`;
+
   return (
-    <header>
-      <nav className="bg-white">
-        <div className="flex justify-between lg:justify-between h-[100px] items-center px-7 2xl:px-[102px] font-poppins max-w-[1500px] mx-auto">
-          <a href="/" className="logo">
-            <img src={Logo} className="w-40 xl:w-56" alt="" />
+    <header
+      className={`fixed top-0 w-full z-20 ${
+        isScrolled ? "bg-white shadow-md" : "bg-transparent"
+      }  transition-all duration-500`}
+    >
+      <nav>
+        <div className="flex justify-between lg:justify-between py-12 max-md:py-8 lg:py-8 items-center px-7 lg:px-[102px] font-poppins mx-auto max-w-[1500px]">
+          <a href={"/"}>
+            <img
+              src={logo}
+              alt="logo"
+              className="relative h-[31px] cursor-pointer z-40"
+            />
           </a>
           {/* Menu */}
           <div className="items-center justify-between hidden menu lg:flex">
             <div>
-              <ul className="flex space-x-10 xl:space-x-[61px]">
-                {menuItems.map((item, index) => {
+              <ul className="flex space-x-10 lg:space-x-6 wideScreen:space-x-[61px] xl:space-x-[30px]">
+                {menuItems.map((item) => {
                   return (
                     <li
-                      key={index}
+                      key={item.key}
                       className="transition-all cursor-pointer hover:scale-105 hover:text-primary"
                     >
                       <a href={item.where}>{item.name}</a>
@@ -27,34 +63,51 @@ const Header = () => {
               </ul>
             </div>
           </div>
-          <div className="hidden transition-all donate md:block hover:scale-110">
+          <div className="hidden transition-all donate lg:block hover:scale-110">
             <button className="bg-primary w-[150px] rounded-[10px] py-3 text-white">
               Donate Now
             </button>
           </div>
           {/* Mobile Menu */}
 
-          <div className="md:hidden">
+          <div className="z-40 lg:hidden" onClick={handleNavClick}>
             <div className="mobile-icon">
-              <img
-                src={HamburgerIcon}
-                alt="menu icon"
-                className="cursor-pointer"
-              />
+              <HamburgerIcon isOpen={isAnimating} />
             </div>
           </div>
         </div>
 
         {/* Tab menu */}
-        <ul className="hidden md:flex lg:hidden justify-between px-7 h-[50px] items-center border-t border-light-purple border-opacity-20 font-poppins">
-          {menuItems.map((item, index) => {
-            return (
-              <li key={index} className="cursor-pointer hover:scale-105">
-                <a href={item.where}>{item.name}</a>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={`lg:hidden z-30 ${navClasses}`}>
+          <div className="py-12 lg:py-8 px-7 sm:px-[62px] xl:px-[102px] mx-auto opacity-0">
+            <img src={logo} alt="logo" className=" h-[31px]" />
+          </div>
+
+          <ul className="px-7 h-[50px] text-left font-poppins">
+            {menuItems.map((item) => {
+              return (
+                <li
+                  key={item.key}
+                  className={`cursor-pointer hover:scale-105 border-light-purple border-opacity-20 py-3 border-dashed border-t-2 text-2xl ${
+                    isAnimating ? "animate-slide-in" : "hidden"
+                  }`}
+                  style={{
+                    animationDelay: `${item.key * 500}ms`,
+                  }}
+                >
+                  <span>
+                    <a href={item.where}>{item.name}</a>
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="absolute w-full transition-all donate bottom-10 hover:scale-110 px-7">
+            <button className="bg-primary w-full rounded-[10px] py-3 text-white">
+              Donate Now
+            </button>
+          </div>
+        </div>
       </nav>
     </header>
   );
--- src/components/Home/Press.tsx
+++ src/components/Home/Press.tsx
@@ -8,34 +8,29 @@ const Press = () => {
         <h2 className='font-poppins font-bold text-[32px] leading-[48px] max-w-2xl mb-[63px]'>
           Get latest updates on the Nigeria Tech Community
         </h2>
-        <div className='grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 gap-5'>
-          <div className='lg:row-span-2 lg:col-span-2 bg-red-500 rounded-lg h-[400px] lg:h-[500px] relative font-dm-sans shadow-lg group hover:scale-105 transition-all cursor-pointer'>
-            <div className='absolute bottom-0 left-0 right-0 h-40 group-hover:h-full transition-all bg-gradient-to-t from-black to-transparent rounded-lg'></div>
+        <div className='grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 gap-5 font-gelion'>
+          <div className='lg:row-span-2 lg:col-span-2 bg-[url("/assets/press-1.svg")] rounded-lg h-[400px] sm:h-[500px] lg:h-[600px] relative font-dm-sans shadow-lg group hover:scale-105 transition-all cursor-pointer bg-no-repeat bg-center bg-cover'>
+            <div className='absolute bottom-0 left-0 right-0 group-hover:h-80 h-full transition-all bg-gradient-to-t from-black to-transparent rounded-lg'></div>
             <div className='absolute bottom-5 flex flex-col space-y-5 p-5'>
-              <div className='news-icon'></div>
-              <div className='title font-bold text-white text-2xl line-clamp-2'>
-                Njoku Emmanuel's Journey To Blockchain Stardom
+              <div className='news-icon'>
+                <img src="/assets/medium.svg" alt="medium icon" />
               </div>
-              <div className='description text-white leading-4 line-clamp-4 lg:line-clamp-6'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                iure provident voluptas distinctio. Praesentium explicabo enim,
-                voluptates voluptatum aliquam pariatur. Laudantium a deserunt
-                omnis incidunt, necessitatibus tenetur eveniet impedit nisi!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Assumenda architecto deserunt itaque cupiditate magnam expedita,
-                exercitationem modi illum veritatis quod autem dolore maiores
-                rem magni repellat. In nisi voluptatem exercitationem.
+              <div className='title font-medium text-[22px] leading-7 lg:leading-[47px] lg:font-bold text-white lg:text-[30px] xl:text-[38px] line-clamp-3'>
+              Rising Stars: How These Few Nigerians in Tech are Putting Nigeria on the World Map
+              </div>
+              <div className='description text-white hidden lg:block line-clamp-3 font-dm-sans text-xl leading-[26.04px]'>
+              In recent years, Nigeria has been making a name for itself in the global tech industry, with an increasing number of Nigerian tech professionals and entrepreneurs gaining recognition and making...
               </div>
             </div>
           </div>
           <div className='lg:col-span-2 bg-blue-500 rounded-lg relative font-dm-sans shadow-lg group hover:scale-105 transition-all cursor-pointer'>
-            <div className='absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-black to-transparent rounded-lg group-hover:h-full transition-all'></div>
+            <div className='absolute bottom-0 left-0 right-0 group-hover:h-40 bg-gradient-to-t from-black to-transparent rounded-lg h-full transition-all'></div>
             <div className='absolute bottom-5 lg:bottom-2 flex flex-col space-y-5 p-5'>
               <div className='news-icon'></div>
-              <div className='title font-bold text-white text-2xl line-clamp-2'>
+              <div className='title font-medium text-[22px] leading-7 lg:leading-[47px] lg:font-bold text-white lg:text-[30px] xl:text-[38px] line-clamp-3'>
                 Njoku Emmanuel's Journey To Blockchain Stardom
               </div>
-              <div className='description text-white leading-4 lg:hidden line-clamp-4 lg:line-clamp-6'>
+              <div className='description text-white hidden line-clamp-3 font-dm-sans text-xl leading-[26.04px]'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
                 iure provident voluptas distinctio. Praesentium explicabo enim,
                 voluptates voluptatum aliquam pariatur. Laudantium a deserunt
@@ -44,13 +39,13 @@ const Press = () => {
             </div>
           </div>
           <div className='relative bg-lime-500 rounded-lg shadow-lg group hover:scale-105 transition-all cursor-pointer'>
-          <div className='absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-black to-transparent rounded-lg group-hover:h-full transition-all'></div>
+          <div className='absolute bottom-0 left-0 right-0 group-hover:h-40 bg-gradient-to-t from-black to-transparent rounded-lg h-full transition-all'></div>
             <div className='absolute bottom-5 lg:bottom-2 flex flex-col space-y-5 p-5'>
               <div className='news-icon'></div>
-              <div className='title font-bold text-white text-2xl line-clamp-2'>
+              <div className='title font-medium text-[22px] leading-7 lg:leading-[47px] lg:font-bold text-white lg:text-[30px] xl:text-[38px] line-clamp-3'>
                 Njoku Emmanuel's Journey To Blockchain Stardom
               </div>
-              <div className='description text-white leading-4 lg:hidden line-clamp-4 lg:line-clamp-6'>
+              <div className='description text-white hidden line-clamp-3 font-dm-sans text-xl leading-[26.04px]'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
                 iure provident voluptas distinctio. Praesentium explicabo enim,
                 voluptates voluptatum aliquam pariatur. Laudantium a deserunt
@@ -59,13 +54,13 @@ const Press = () => {
             </div>
           </div>
           <div className='relative bg-orange-500 rounded-lg shadow-lg group hover:scale-105 transition-all cursor-pointer'>
-          <div className='absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-black to-transparent rounded-lg group-hover:h-full transition-all'></div>
+          <div className='absolute bottom-0 left-0 right-0 group-hover:h-40 bg-gradient-to-t from-black to-transparent rounded-lg :h-full transition-all'></div>
             <div className='absolute bottom-5 lg:bottom-2 flex flex-col space-y-5 p-5'>
               <div className='news-icon'></div>
-              <div className='title font-bold text-white text-2xl line-clamp-2'>
+              <div className='title font-medium text-[22px] leading-7 lg:leading-[47px] lg:font-bold text-white lg:text-[30px] xl:text-[38px] line-clamp-3'>
                 Njoku Emmanuel's Journey To Blockchain Stardom
               </div>
-              <div className='description text-white leading-4 lg:hidden line-clamp-4 lg:line-clamp-6'>
+              <div className='description text-white hidden line-clamp-3 font-dm-sans text-xl leading-[26.04px]'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
                 iure provident voluptas distinctio. Praesentium explicabo enim,
                 voluptates voluptatum aliquam pariatur. Laudantium a deserunt
