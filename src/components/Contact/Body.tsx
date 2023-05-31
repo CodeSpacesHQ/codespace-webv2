4be9a4c23b0f59c45d500805c48e69fb39e3990a
Clinton Imaro <93707197+clintonimaroo@users.noreply.github.com>
2023-05-31 09:57:29 +0200

    Update Body.tsx

--- src/components/Contact/Body.tsx
+++ src/components/Contact/Body.tsx
@@ -110,7 +110,7 @@ const ContactBody: React.FC = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-12"
                 id="name"
                 type="text"
-                placeholder="Your name"
+                placeholder="Enter your full name"
                 value={values.Name}
                 onChange={handleChange}
                 name="Name"
@@ -135,12 +135,12 @@ const ContactBody: React.FC = () => {
                 className="block text-sm font-medium mb-2"
                 htmlFor="project"
               >
-                How can we help?
+                Message
               </label>
               <textarea
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-h-[128px]"
                 id="project"
-                placeholder="Tell us a little about the project..."
+                placeholder="Write us something....."
                 value={values.project}
                 onChange={handleChange}
                 name="project"
@@ -151,7 +151,7 @@ const ContactBody: React.FC = () => {
                 className="block text-sm font-medium mb-5"
                 htmlFor="purpose"
               >
-                Purpose
+                Why are you contacting us
               </label>
               <div className="grid grid-cols-2 gap-4">
                 {formPurpose.map(({ key, value }) => (
