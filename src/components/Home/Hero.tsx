6c73a8143fd647e357c594314d93e43aac8ba168
Bright Lazarus <lazbright1@gmail.com>
2023-06-12 15:23:05 +0100

    Update Header.tsx

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="relative bg-gradient-to-b from-[#F8F8FD] to-white sm:min-h-[calc(100vh+400px)] lg:min-h-[calc(100vh+820px)] xl:min-h-[calc(100vh+100px)] wideScreen:min-h-[calc(100vh+200px)] overflow-hidden">
       <div className="bg-[url('/assets/Elements.png')] bg-no-repeat bg-left bg-cover">
-        <div className="pt-[188px] sm:pt-[251px] max-w-[1500px] mx-auto">
+        <div className="pt-[136px] max-w-[1500px] mx-auto">
           <div className="px-7 sm:px-16 lg:px-[65px] md:py-0 xl:pl-[101px] relative ">
             <Bounce cascade damping={0.4} duration={2000}>
               <h1 className="max-w-2xl lg:max-xl:max-w-xl font-gelion font-bold text-4xl/[42px] md:text-[60px]/[70px] lg:max-xl:text-5xl/[60px] mb-9">
@@ -39,7 +39,7 @@ const Hero = () => {
       <img
         className="absolute max-md:right-0 max-md:hidden max-md:rotate-180 sm:left-0 max-lg:w-[200px] max-lg:bottom-8 lg:bottom-10 animate-pulse"
         src="/assets/arrow.svg"
-        alt=""
+        alt="icons"
       />
     </section>
   );
