59ac3957d4631c001e2d36332d0cb6f107eed4e0
Clinton Imaro <93707197+clintonimaroo@users.noreply.github.com>
2023-06-12 22:20:39 +0200

    update about us

--- src/components/Perks/Hero.tsx
+++ src/components/Perks/Hero.tsx
@@ -12,7 +12,7 @@ export const PerksHero = () => {
         className="fixed z-[100] w-full h-1 bg-primary top-0 max-lg:hidden"
       />
       <Hero
-        title="Perks and Benefits"
+        title="PERKS AND BENEFITS"
         h1="Jump on a shopping spree and get amazing discounts from our partners today!"
         hero={image}
       />
